29155677a6e48e0922c061ebcf750314f1c6459b
Aleksandr <sas2job@gmail.com>
2019-03-29 14:03:08 +0300

    Change color to green and change text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['Hello World!', 'Welcome to my site!'], 'text',['green','green',]);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
